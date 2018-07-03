8520ab426b385387bbdab8c749e942a3cfc3a496
Cristian J. Rosario <cris0u0rosario@gmail.com>
2018-07-02 23:53:56 -0400

    UpdateForSortingAndTest

--- LittleJohnsHutsPizzaPie/LittleJohnsHutsPizzaPie/Functions/Sorting.cs
+++ LittleJohnsHutsPizzaPie/LittleJohnsHutsPizzaPie/Functions/Sorting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LittleJohnsHutsPizzaPie.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,25 @@ namespace LittleJohnsHutsPizzaPie.Functions
 {
     public class Sorting
     {
+        public static List<Order> OrderEarlest(List<Order> list)
+        {
+            list.Sort((p1, p2) => DateTime.Compare(p1.DateOrder, p2.DateOrder));
+            return list;
+        }
+        public List<Order> OrderLatest(List<Order> list)
+        {
+
+            return list;
+        }
+        public static List<Order> OrderChepest(List<Order> list)
+        {
+            list = list.Sort((x, y) => x.price > y.price);
+            return list;
+        }
+        public static  List<Order> OrderMostExpencive(List<Order> list)
+        {
+            return list;
+        }
+
     }
 }
